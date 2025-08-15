d893d820f5a9200532bae6f29d5b6be2afd9fbf3
tashaxanavsem82 <swgwull@hotmail.com>
2025-08-14 23:22:39 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Team Task Manager</h1>
+        <p>Manage your tasks collaboratively.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
