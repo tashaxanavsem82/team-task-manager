8cb587e012e98b78ebadd435e6038b48daff0811
tashaxanavsem82 <swgwull@hotmail.com>
2025-08-14 23:39:55 -0500

    Add comment to explain error handling middleware

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Error handling middleware
+// This middleware catches errors from previous middleware and returns a 500 status with a message.
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
